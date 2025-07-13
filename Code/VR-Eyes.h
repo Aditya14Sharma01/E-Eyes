474c531d62c36be7919571767935c505e3418610
Aditya Sharma <aditya1401sharma@gmail.com>
2025-07-13 18:08:59 +0530

    code updated

---
+++ Code/VR-Eyes.h
@@ -0,0 +1,17 @@
+const char MAIN_page[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+ <head>
+   <title>VR eyes</title>
+ </head>
+ <body bgcolor="#0A122A" text="#DF7401">
+<center>
+
+<img src="http://192.168.4.1/mjpeg/1" alt="leftEYE" width=430>
+
+<img src="http://192.168.4.1/mjpeg/1" alt="leftEYE" width=430>
+</center>
+</body>
+</html>
+
+)=====";
